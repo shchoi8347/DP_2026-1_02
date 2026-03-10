28fb132a33c7fd8ed248b9ae3e74aa900735680d
shchoi8347 <csh@duksung.ac.kr>
2026-03-10 11:46:48 +0900

    3월10일 수업종료(02반)

---
+++ src/practice/ch01/Main.java
@@ -0,0 +1,9 @@
+// Created: 2026-02-23 14:30
+package practice.ch01;
+
+public class Main {
+    public static void main(String[] args) {
+        Book book = new Book("Java Programming");
+        System.out.println("Book name: " + book.getName());
+    }
+}
